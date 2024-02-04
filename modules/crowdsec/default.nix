9f1c60c461cecd1eb1658283ed05bb0de44e1388
InaccurateTank <inaccuratetank@outlook.com>
2024-02-04 14:57:04 -0800

    strings...

--- modules/crowdsec/default.nix
+++ modules/crowdsec/default.nix
@@ -19,44 +19,44 @@ in {
     environment.etc = {
       "crowdsec/config.yaml" = {
         source = "${store}/share/crowdsec/config/config.yaml";
-        mode = 0600;
+        mode = "0600";
       };
       "crowdsec/dev.yaml" = {
         source = "${store}/share/crowdsec/config/dev.yaml";
-        mode = 0644;
+        mode = "0644";
       };
       "crowdsec/user.yaml" = {
         source = "${store}/share/crowdsec/config/user.yaml";
-        mode = 0644;
+        mode = "0644";
       };
       "crowdsec/acquis.yaml" = {
         source = "${store}/share/crowdsec/config/acquis.yaml";
-        mode = 0644;
+        mode = "0644";
       };
       "crowdsec/profiles.yaml" = {
         source = "${store}/share/crowdsec/config/profiles.yaml";
-        mode = 0644;
+        mode = "0644";
       };
       "crowdsec/simulation.yaml" = {
         source = "${store}/share/crowdsec/config/simulation.yaml";
-        mode = 0644;
+        mode = "0644";
       };
       "crowdsec/console.yaml" = {
         source = "${store}/share/crowdsec/config/console.yaml";
-        mode = 0644;
+        mode = "0644";
       };
       "crowdsec/context.yaml" = {
         source = "${store}/share/crowdsec/config/context.yaml";
-        mode = 0644;
+        mode = "0644";
       };
       "crowdsec/patterns".source = "${store}/share/crowdsec/config/patterns";
       "crowdsec/local_api_credentials.yaml" = {
         source = "${store}/share/crowdsec/config/local_api_credentials.yaml";
-        mode = 0600;
+        mode = "0600";
       };
       "crowdsec/online_api_credentials.yaml" = {
         source = "${store}/share/crowdsec/config/online_api_credentials.yaml";
-        mode = 0600;
+        mode = "0600";
       };
     };
 
